3778c469bcf5b98fed448cb03e74f0c332893626
tgeronimo <thi.matheus@gmail.com>
2021-11-21 21:08:36 -0300

    created a custom error message that will inform a timestamp and the message from the exception

--- src/main/java/org/mathero/restapi/ui/exceptions/AppExceptionsHandler.java
+++ src/main/java/org/mathero/restapi/ui/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,6 @@
 package org.mathero.restapi.ui.exceptions;
 
+import org.mathero.restapi.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +9,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
 @ControllerAdvice
 public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request) {
+
+        String errorMessageDescription = ex.getLocalizedMessage();
+        if (errorMessageDescription == null) errorMessageDescription = ex.toString();
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
         return new ResponseEntity<>(
-                ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR
+                errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
 }
